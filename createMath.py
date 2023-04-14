567b1ec8ef3fc68e8715a09c623e1257f14230d3
lich-wang <lich.wang@gmail.com>
2023-04-14 13:32:33 +0800

    maths

--- createMath.py
+++ createMath.py
@@ -1,10 +1,29 @@
-#循环遍历的方式,生成指定范围内加减乘除练习题.
+import itertools 
 
-#计算后的最大结果
-max_result = 100
-#计算次数 
+# 计算后的最大结果
+max_num = 100
+# 计算次数
 calc_step = 2
-#计算类型
-calc_type = {'+','-'}
+# 计算类型
+#calc_type = {'+', '-', '*', '/'}
+calc_type = {'+', '-'}
 
+def generate_expressions(numbers, operators, depth, current_depth=1):
+    if current_depth == depth:
+        for num1, num2, op in itertools.product(numbers, numbers, operators):
+            yield f"{num1} {op} {num2}"
+    else:
+        for num, sub_expression, op in itertools.product(numbers, generate_expressions(numbers, operators, depth, current_depth + 1), operators):
+            yield f"{num} {op} {sub_expression}"
 
+def is_valid_expression(expression, max_num):
+    try:
+        result = eval(expression)
+        return result > 10 and result < max_num and isinstance(result, int)
+    except (ValueError, ZeroDivisionError):
+        return False
+
+# 根据 calc_step 生成表达式
+for expression in generate_expressions(range(0, max_num), calc_type, calc_step):
+    if is_valid_expression(expression, max_num):
+        print(expression)
